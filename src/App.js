61b7341124a0d7aa507cd18e1985ce1496ef1786
Gilbert Rosario <pro.devgilbert@gmail.com>
2018-12-19 19:09:03 -0400

    Finished styling UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import Axios from "axios";
 import Header from "./components/header";
 import Card from "./components/card";
+import config from "./config.json";
+import "react-toastify/dist/ReactToastify.css";
 import "./sass/main.scss";
 
 class App extends Component {
@@ -10,15 +13,19 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const { data } = await Axios.get(
-      "https://s3-sa-east-1.amazonaws.com/config.instacarro.com/recruitment/auctions.json"
-    );
-    this.setState({ data });
+    try {
+      const { data } = await Axios.get(config.corsProxy + config.apiUrl);
+      this.setState({ data });
+    } catch (ex) {
+      console.log("Logging the error", ex);
+      toast.error("An unexpected error ocurred.");
+    }
   }
 
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <Header />
         <main className="flex-grid">
           <Card data={this.state.data} />
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,23 +4,40 @@ const Card = ({ data }) => {
   return (
     <React.Fragment>
       {data.map(c => (
-        <div className="card">
+        <div className="card" key={c.id}>
           <div className="card__thumbnail">
             <img src={c.imageUrl} alt={c.make} className="card__img" />
             <div className="card__thumbnail--details">ver detalhes</div>
           </div>
-          <div className="card__description">
-            <div className="card__row">
-              <div className="card__column">
-                <p className="card__column--title">Tempo restante</p>
-                {c.remainingTime}
+          <div className="card__details">
+            <div className="group-row group-row--flex">
+              <div className="group-row__col">
+                <p className="group-row__title">Tempo restante</p>
+                <p className="group-row__text-color group-row__text-color--orange">
+                  15:55:55
+                </p>
+              </div>
+              <div className="group-row__vl">&nbsp;</div>
+              <div className="group-row__col">
+                <p className="group-row__title">Ultima oferta</p>
+                <p className="group-row__text-color group-row__text-color--green">
+                  R$ 29.250
+                </p>
               </div>
             </div>
-            <div className="card__row">
-              <p className="card__desc">{`${c.make} ${c.model} ${c.version} ${
-                c.year
-              }`}</p>
+            <div className="group-row">
+              <p className="group-row__desc">
+                {`${c.make} ${c.model} ${c.version} ${c.year}`}
+              </p>
             </div>
+            <div className="group-row group-row--flex">
+              <div className="group-row__col">{c.year}</div>
+              <div className="group-row__vl">&nbsp;</div>
+              <div className="group-row__col">{`${c.km} km`}</div>
+            </div>
+            <button type="button" className="btn btn--card">
+              Fazer oferta
+            </button>
           </div>
         </div>
       ))}
